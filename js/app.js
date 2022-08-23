4dec6d02ddfb30cb641fa3b078d7aad9b0cf27bb
Erwin Jongenelen <erwinjongenelen@gmail.com>
2022-08-23 22:58:22 +0200

    Added interface function and added main function for the game

--- js/app.js
+++ js/app.js
@@ -1,18 +1,21 @@
 const game = document.getElementById('game');
-const sweetspotRange = 10;
-const hotzone = 30;
+
+// Settings
+const SWEETSPOT_RANGE = 10;
+const HOTZONE = 30;
+let UNLOCK_ZONE = {};
 
 // Initialize lockpicks and lockpick health
 let LOCKPICKS = 3;
 let LOCKPICK_HEALTH = 100;
 
 // Generate sweetspot and hotzone in degrees (between -90 and 90, by using floats)
-const generateUnlockZone = () => {
+const generateUnlockZone = async () => {
 
-    const sweetspotStart = parseFloat((Math.random() * ((90 - sweetspotRange) - -90) + -90).toFixed(2));
-    const sweetspotEnd = sweetspotStart + sweetspotRange;
-    const hotzoneStart = sweetspotStart - hotzone;
-    const hotzoneEnd = sweetspotEnd + hotzone;
+    const sweetspotStart = parseFloat((Math.random() * ((90 - SWEETSPOT_RANGE) - -90) + -90).toFixed(2));
+    const sweetspotEnd = sweetspotStart + SWEETSPOT_RANGE;
+    const hotzoneStart = sweetspotStart - HOTZONE;
+    const hotzoneEnd = sweetspotEnd + HOTZONE;
 
     return {
         'hotzone_start': hotzoneStart,
@@ -23,9 +26,33 @@ const generateUnlockZone = () => {
 
 }
 
-console.log( generateUnlockZone() );
-
 // Initialize scene: Background, outer lock, lock, lockpick, kind of lock, amount of lockpicks left
+const setupInterface = () => {
+
+    // Lock
+    const outerLock = document.createElement('div');
+    const lock = document.createElement('div');
+    const lockImage = document.createElement('img');
+
+    outerLock.id = 'outerlock';
+    lock.id = 'lock';
+    lockImage.id = 'lock-image';
+    lockImage.src = './assets/inner-lock.png';
+
+    // Lockpick
+    const lockpickContainer = document.createElement('div');
+    const lockpick = document.createElement('div');
+
+    lockpickContainer.id = 'lockpick-container';
+    lockpick.id = 'lockpick';
+
+    // Append elements to #game
+    lockpickContainer.append(lockpick);
+    lock.append(lockImage, lockpickContainer);
+    outerLock.append(lock);
+    game.append(outerLock);
+
+}
 
 // Setup mouse controls to move lockpick between -90 and 90 degrees
 
@@ -40,3 +67,15 @@ console.log( generateUnlockZone() );
 // Setup mouse release function to pause lockpick health and lock rotates back to original state
 
 // On lockpick rotation and is in the sweetspot, player wins
+
+const start = async () => {
+
+    // Set UNLOCK_ZONE global variable
+    UNLOCK_ZONE = await generateUnlockZone();
+
+    // Setup interface
+    setupInterface();
+
+}
+
+start();
